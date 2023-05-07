873b1e632d6ad5c49602dec28ba08a9ccc163739
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-05-06 20:56:58 -0500

    refactor(Root): working in basic styles
    Were imported styles from the water.css online webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import './App.css';
 
+const URL = 'https://www.omdbapi.com/?apikey=73204d82&s=ave';
 function App() {
-
   return (
-    <>
-      <h1>Screen Scout</h1>
-    </>
-  )
+    <div className="page">
+      <header>
+        <h1>Screen Scout</h1>
+        <form className="form">
+          <input type="text" placeholder="Avatar, Star Wars, Avengers ..." />
+          <button>Search</button>
+        </form>
+      </header>
+      <main>Results</main>
+    </div>
+  );
 }
 
-export default App
+export default App;
