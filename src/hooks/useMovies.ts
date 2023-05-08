f3c2aea1df55974678d20594c906de45c288f92f
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-05-07 19:53:33 -0500

    perf(Root): avoid repeating the same query search
    is implemented useRef Hooks to avoid repeating the same query when the user is tracking for the
    
    movies search

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Movie } from '../models';
 import { searchMovies } from '../services';
 
@@ -6,11 +6,14 @@ export function useMovies({ search }: { search: string }) {
   const [movies, setMovies] = useState<Movie[] | null>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const prevSearch = useRef(search);
 
   const getMovies = async () => {
     try {
+      if (prevSearch.current === search) return;
       setIsLoading(true);
       setError(null);
+      prevSearch.current = search;
       const newMovies = await searchMovies({ search });
       return setMovies(newMovies);
     } catch (error: Error | unknown) {
