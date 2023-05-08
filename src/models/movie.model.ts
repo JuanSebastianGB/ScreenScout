28c9144a57e7b50857a9e45fb8f308e6bcf8760e
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-05-07 19:34:32 -0500

    chore(Root): Defining model Core
    Were defined the models for the API response and for the business logic

--- src/models/movie.model.ts
+++ src/models/movie.model.ts
@@ -1,13 +1,20 @@
-export interface Movie {
-  Search: Search[];
+export interface Response {
+  Search: MovieApiResponse[];
   totalResults: string;
   Response: string;
 }
 
-export interface Search {
+export interface MovieApiResponse {
   Title: string;
   Year: string;
   imdbID: string;
   Type: string;
   Poster: string;
+}
+
+export interface Movie {
+  title: string;
+  year: string;
+  id: string;
+  image: string;
 }
